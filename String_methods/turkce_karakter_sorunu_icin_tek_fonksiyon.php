2514f956e294136b75e32009a6489ccf68745d82
Ali KIZILARSLAN <ali.kizilarslan@hotmail.com>
2023-01-24 00:11:20 +0200

    Create turkce_karakter_sorunu_icin_tek_fonksiyon.php
    Php deki hazır fonksiyonların yetmediği türkçe karakter (strtolower, lcfirst) sorunları çözmek için yazılan özel function.

---
+++ String_methods/turkce_karakter_sorunu_icin_tek_fonksiyon.php
@@ -0,0 +1,39 @@
+<?php
+
+function ConvertLetter($text,$case)
+{
+  $replaceLower = array("ç", "ğ", "ı", "i", "ö", "ş", "ü");
+  $replaceUpper = array("Ç", "Ğ", "I", "İ", "Ö", "Ş", "Ü");
+  
+  if($case == "upper")
+  {
+    $edit = str_replace($replaceLower, $replaceUpper, $text);
+    $edit = strtoupper($edit);
+  }elseif($case == "lower")
+  {
+    $edit = str_replace($replaceUpper, $replaceLower, $text);
+    $edit = strtolower($edit);
+  }elseif($case == "firstUpper")
+  {
+    $length = strlen($text);
+    $firstLetter = mb_substr($text, 0, 1, "UTF-8");
+    $otherLetter = mb_substr($text, 1, $length, "UTF-8");
+    $editFL = str_replace($replaceL, $replaceU, $firstLetter);
+    $editFL = strtoupper($editFL);
+    $edit = $editFL.$otherLetter;
+  }elseif($case == "firstLower")
+  {
+    $length = strlen($text);
+    $firstLetter = mb_substr($text, 0, 1, "UTF-8");
+    $otherLetter = mb_substr($text, 1, $length, "UTF-8");
+    $editFL = str_replace($replaceU, $replaceL, $firstLetter);
+    $editFL = strtolower($editFL);
+    $edit = $editFL.$otherLetter;
+  }
+  
+  return $edit;
+}
+
+
+
+?>
