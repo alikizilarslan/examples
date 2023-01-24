9512d439b19ecee11d4d8e49b5182105525a49c2
Ali KIZILARSLAN <ali.kizilarslan@hotmail.com>
2023-01-24 20:48:27 +0300

    Türkçe büyük-küçük harf dönüştürme class

--- String_methods/turkce_karakter_sorunu_icin_tek_fonksiyon.php
+++ String_methods/turkce_karakter_sorunu_icin_tek_fonksiyon.php
@@ -1,37 +1,84 @@
 <?php
 
-function ConvertLetter($text,$case)
+class convertletter
 {
-  $replaceLower = array("ç", "ğ", "ı", "i", "ö", "ş", "ü"); //Dönüştürülecek türkçe-küçük karakter dizisi
-  $replaceUpper = array("Ç", "Ğ", "I", "İ", "Ö", "Ş", "Ü"); //Dönüştürülecek türkçe-büyük karakter dizisi
-  
-  if($case == "upper")
-  {
-    $edit = str_replace($replaceLower, $replaceUpper, $text);
-    $edit = strtoupper($edit);
-  }elseif($case == "lower")
-  {
-    $edit = str_replace($replaceUpper, $replaceLower, $text);
-    $edit = strtolower($edit);
-  }elseif($case == "firstUpper")
-  {
-    $length = strlen($text);
-    $firstLetter = mb_substr($text, 0, 1, "UTF-8");
-    $otherLetter = mb_substr($text, 1, $length, "UTF-8");
-    $editFL = str_replace($replaceL, $replaceU, $firstLetter);
-    $editFL = strtoupper($editFL);
-    $edit = $editFL.$otherLetter;
-  }elseif($case == "firstLower")
-  {
-    $length = strlen($text);
-    $firstLetter = mb_substr($text, 0, 1, "UTF-8");
-    $otherLetter = mb_substr($text, 1, $length, "UTF-8");
-    $editFL = str_replace($replaceU, $replaceL, $firstLetter);
-    $editFL = strtolower($editFL);
-    $edit = $editFL.$otherLetter;
-  }
-  
-  return $edit; 
+    public $replaceLower = ['ç', 'ğ', 'ı', 'i', 'ö', 'ş', 'ü']; //Dönüştürülecek türkçe-küçük karakter dizisi
+    public $replaceUpper = ['Ç', 'Ğ', 'I', 'İ', 'Ö', 'Ş', 'Ü']; //Dönüştürülecek türkçe-büyük karakter dizisi
+    
+    public function upper($text)
+    {
+        $edit = str_replace($this->replaceLower, $this->replaceUpper, $text);
+        $edit = strtoupper($edit);
+        return $edit;
+    }
+    public function lower($text)
+    {
+        $edit = str_replace($this->$replaceUpper, $this->$replaceLower, $text);
+        $edit = strtolower($edit);
+        return $edit;
+    }
+    public function firstupper($text)
+    {
+        $length = strlen($text);
+        $firstLetter = mb_substr($text, 0, 1, "UTF-8");
+        $otherLetter = mb_substr($text, 1, $length, "UTF-8");
+        $editFL = str_replace($this->$replaceLower, $this->$replaceUpper, $firstLetter);
+        $editFL = strtoupper($editFL);
+        $edit = $editFL.$otherLetter;
+        return $return;
+    }
+    public function firstlower($text)
+    {
+        $length = strlen($text);
+        $firstLetter = mb_substr($text, 0, 1, "UTF-8");
+        $otherLetter = mb_substr($text, 1, $length, "UTF-8");
+        $editFL = str_replace($this->$replaceUpper, $this->$replaceLower, $firstLetter);
+        $editFL = strtolower($editFL);
+        $edit = $editFL.$otherLetter;
+        return $edit;
+    }
+    public function wordupper($text)
+    {
+        $arr = explode(" ",$text);
+        $str = "";
+        for($i=0; $i<count($arr); $i++)
+        {
+            $length = strlen($arr[$i]);
+            $wordUpper = mb_substr($arr[$i], 0, 1, "UTF-8");
+            $otherLetter = mb_substr($arr[$i], 1, $length, "UTF-8");
+            $editFL = str_replace($this->replaceLower, $this->replaceUpper, $wordUpper);
+            $editFL = strtoupper($editFL);
+            $edit = $editFL.$otherLetter;
+            if($i == count($arr)-1)
+            {
+                $str .= $edit;
+            }else{
+                $str .= $edit." ";
+            }
+        }
+        return $str;
+    }
+    public function wordlower($text)
+    {
+        $arr = explode(" ",$text);
+        $str = "";
+        for($i=0; $i<count($arr); $i++)
+        {
+            $length = strlen($arr[$i]);
+            $wordLower = mb_substr($arr[$i], 0, 1, "UTF-8");
+            $otherLetter = mb_substr($arr[$i], 1, $length, "UTF-8");
+            $editFL = str_replace($this->replaceUpper, $this->replaceLower, $wordLower);
+            $editFL = strtolower($editFL);
+            $edit = $editFL.$otherLetter;
+            if($i == count($arr)-1)
+            {
+                $str .= $edit;
+            }else{
+                $str .= $edit." ";
+            }
+        }
+        return $str;
+    } 
 }
 
 
